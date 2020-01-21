c2680037640708ce3f24ea4487ac8410d56af141
igorsuz <igorsuz1984@gmail.com>
2020-01-21 09:23:24 +0200

    homework_9

--- src/homework_9/homework_9.js
+++ src/homework_9/homework_9.js
@@ -1,1 +1,67 @@
-import './homework_9.scss';
+import "./homework_9.scss";
+const trafficLighterRoot = document.querySelector(".traffic-lighter");
+const lights = trafficLighterRoot.querySelectorAll(".traffic-lighter__light");
+const togglerBtn = trafficLighterRoot.querySelector(
+  ".traffic-lighter__toggler"
+);
+let isEnabled = false;
+let activeIndex = 0;
+let intervalId = 0;
+
+const toggleOffAll = () => {
+  for (let i = 0; i < lights.length; i++) {
+    lights[i].classList.remove("active");
+  }
+};
+const activateLight = () => {
+  lights[activeIndex].classList.add("active");
+};
+
+for (let i = 0; i < lights.length; i++) {
+  lights[i].onclick = function() {
+    if (isEnabled) {
+      clearInterval(intervalId);
+      toggleOffAll();
+      activeIndex = i;
+      activateLight();
+      startAutoChange();
+    }
+  };
+}
+
+const changeIndex = () => {
+  if (activeIndex + 1 < lights.length) {
+    activeIndex += 1;
+  } else {
+    activeIndex = 0;
+  }
+};
+
+const toggleOffLighter = () => {
+  isEnabled = false;
+  toggleOffAll();
+  activeIndex = 0;
+  clearInterval(intervalId);
+};
+const startAutoChange = () => {
+  intervalId = setInterval(() => {
+    toggleOffAll();
+    changeIndex();
+    activateLight();
+  }, 1000);
+};
+const toggleOnLighter = () => {
+  isEnabled = true;
+  activateLight(0);
+  startAutoChange();
+};
+
+const togglerClickHandler = () => {
+  if (isEnabled) {
+    toggleOffLighter();
+  } else {
+    toggleOnLighter();
+  }
+};
+
+togglerBtn.onclick = togglerClickHandler;
